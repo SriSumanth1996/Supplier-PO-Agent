1b4ca199001b08c6b67d8f90a1cbfc583fe83f0a
SriSumanth1996 <118508129+SriSumanth1996@users.noreply.github.com>
2025-08-11 13:47:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import Flow
 from email.mime.text import MIMEText
 import base64 as b64
 from datetime import datetime, timedelta
